0bf1c419ee3bef22bbf0b55eba287b23e80b4cd4
Andreas Hallberg <andreas.hallberg@infor.com>
2023-07-31 10:19:24 +0200

    add modal

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import "ids-enterprise-wc/components/ids-button/ids-button";
 import "ids-enterprise-wc/components/ids-container/ids-container";
 import "ids-enterprise-wc/components/ids-hyperlink/ids-hyperlink";
+import "ids-enterprise-wc/components/ids-modal-button/ids-modal-button";
+import "ids-enterprise-wc/components/ids-modal/ids-modal";
 import "ids-enterprise-wc/components/ids-text/ids-text";
 import "ids-enterprise-wc/components/ids-theme-switcher/ids-theme-switcher";
 
 import "./style.css";
 
-setupNetworkStatusCheck(document.getElementById("network-status")!);
+import type IdsButton from "ids-enterprise-wc/components/ids-button/ids-button";
+import type IdsModal from "ids-enterprise-wc/components/ids-modal/ids-modal";
+import type IdsText from "ids-enterprise-wc/components/ids-text/ids-text";
+
+setupNetworkStatusCheck(document.getElementById("network-status") as IdsText);
 if (import.meta.env.PROD) {
   console.log("Registering service worker...");
   setupServiceWorker();
 }
 
-function setupNetworkStatusCheck(element: HTMLElement) {
+setupModal(
+  document.getElementById("modal") as IdsModal,
+  document.getElementById("modal-trigger") as IdsButton
+);
+
+function setupNetworkStatusCheck(element: IdsText) {
   const onlineText = "You are online 🟢";
   const offlineText = "You are offline 🔴";
 
@@ -42,4 +53,16 @@ async function setupServiceWorker() {
     );
     console.log("Service worker registered:", registration);
   }
+}
+
+function setupModal(modal: IdsModal, trigger: IdsButton) {
+  const okButton = modal.querySelector("#button-ok") as HTMLButtonElement;
+
+  trigger.addEventListener("click", () => {
+    modal.show();
+  });
+
+  okButton.addEventListener("click", () => {
+    modal.hide();
+  });
 }
